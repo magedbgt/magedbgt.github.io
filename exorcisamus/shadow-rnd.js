062abdc4ca057eb8d7e4b2fa5be389ce87e91f69
magedbgt <magedbgt@gmail.com>
2019-09-28 03:15:56 -0300

    randomização de artes

---
+++ exorcisamus/shadow-rnd.js
@@ -0,0 +1,31 @@
+//randomização e acesso
+function rnd(array){
+
+  return array[Math.floor(Math.random()*array.length)];
+
+}
+
+window.addEventListener("load", randomArt, false);
+function randomArt(){
+  let homeHeader = document.getElementById('homeHeader'),
+  artes = [
+    'https://magedbgt.github.io/exorcisamus/img/Assinatura.png',
+    'https://magedbgt.github.io/exorcisamus/img/PRA-MAGE.png',
+    'https://magedbgt.github.io/exorcisamus/img/ab1.jpg',
+    'https://magedbgt.github.io/exorcisamus/img/hode.png',
+    'https://magedbgt.github.io/exorcisamus/img/mage.jpg',
+    'https://magedbgt.github.io/exorcisamus/img/photo%20-%20Copy.png',
+    'https://magedbgt.github.io/exorcisamus/img/roses_by_mage.png',
+    'https://magedbgt.github.io/exorcisamus/img/roses_by_Hanano.jpg',
+    'https://magedbgt.github.io/exorcisamus/img/chocotan_by_Hana.jpg',
+  ],
+  //randomização
+  arte = rnd(artes);
+
+  //innerHTML
+  homeHeader.innerHTML =
+  'div.abimg{'+
+            +'background-image: -o-linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('+arte+')!important;'+
+            +'background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.3)), to(rgba(0, 0, 0, 0.3))), url('+arte+')!important;'+
+            +'background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('+arte+')!important;}'
+}
