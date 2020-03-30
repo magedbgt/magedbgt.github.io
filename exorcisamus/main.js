ee7131cc9e60ca982a3f269b783b4d08deda0a6b
Maria Eugenia <magedbgt@gmail.com>
2020-03-30 01:46:18 -0300

    nav mobile expansible

--- exorcisamus/main.js
+++ exorcisamus/main.js
@@ -104,3 +104,19 @@ if (homeHeader != null) {
 };
 
 }
+
+
+let coll = document.getElementsByClassName("collapsible"),
+counter;
+
+for (counter = 0; counter < coll.length; counter++) {
+  coll[counter].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
