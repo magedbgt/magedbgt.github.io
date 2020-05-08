19471333bf44b39601002a388125f2b5c9cad846
Maria Eugenia <magedbgt@gmail.com>
2020-05-08 16:16:20 -0300

    Versão desktop 2 - projetos
    Mudança das imagens apresentadas em projetos e fundo estilo desktop

--- projetos.js
+++ projetos.js
@@ -31,11 +31,19 @@ const link_projetos = [
 ];
 
 const src_projetos = [
-  'img/ba.png',
-  'img/exorcisamus.png',
-  'img/mvt.png',
-  'img/gefro.png'
+  'img/ba-mob.jpeg',
+  'img/exorcisamus-mob.jpeg',
+  'img/mvt-mob.jpeg',
+  'img/gefro-mob.jpeg'
 ];
+
+const src_proj_body = [
+  '../img/ba-desktop.jpg',
+  '../img/exorcisamus-desktop.jpg',
+  '../img/mvt-desktop.jpg',
+  '../img/gefro-desktop.jpg'
+];
+
 const descricoes_projetos = [
   'Site para o escritório Borgato Advocacia em Jaú.',
   'Blog de guias e notícias para Arcebispos Exorcistas no jogo Ragnarok Online.'+parag+'De minha autoria.',
@@ -50,6 +58,8 @@ function showProjeto(el){
   descP = document.getElementById('projeto_p'),
   hashP = document.getElementById('hashtag_projeto');
   let index = el.name;
+  let body_proj = document.getElementById('body_proj');
+  let section_work = document.getElementById('section_work');
 
   panel.classList.remove('seen');
   // panel.style.opacity = '0';
@@ -59,6 +69,8 @@ function showProjeto(el){
     hashP.innerHTML = hash_projetos[index];
     linkP.href = link_projetos[index];
     descP.innerHTML = descricoes_projetos[index];
+    section_work.style.backgroundImage = 'linear-gradient(rgba(22, 22, 22, 1), rgba(22, 22, 22, 0.8), rgba(22, 22, 22, 0.5),rgba(22, 22, 22, 0))';
+    body_proj.style.backgroundImage = 'linear-gradient(rgba(22, 22, 22, 0.7), rgba(22, 22, 22, 0.99)), url('+src_proj_body[index]+')';
     panel.classList.add('seen');
   }, 700);
 }
